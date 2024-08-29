89d053b13d5489a5e28eaf1bf7a8081796ead87f
Sandeep Kumar <165152286+07-Sandeep@users.noreply.github.com>
2024-08-30 00:09:04 +0530

    Add files via upload

---
+++ Landing Pages/Landing Page 6/script.js
@@ -0,0 +1,23 @@
+gsap.from("#part1-img1",{
+  opacity: 0,
+  duration: 1,
+  delay: 0.4,
+  y: 60.
+})
+gsap.from("#part1-img2",{
+  opacity: 0,
+  duration: 1,
+  delay: 0.4,
+  x: 60.
+})
+gsap.from("#part1-img3",{
+  opacity: 0,
+  duration: 1,
+  delay: 0.4,
+  y: -60.
+})
+gsap.from("#site-part1 h1",{
+  opacity: 0,
+  duration: 1,
+  delay: 0.4,
+})
